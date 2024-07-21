cc6409ded42dc16b7a039b0c6ba8a1bae62a17ef
patelsunny <s.patel25495@yahoo.in>
2024-07-21 10:10:43 -0400

    Models created for Store app

---
+++ store/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    brand = models.CharField(max_length=255, default="un-branded")
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=255)
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    image = models.ImageField(upload_to='images/')
+
+    class Meta:
+        verbose_name_plural = "Products"
+
+    def __str__(self):
+        return self.title
